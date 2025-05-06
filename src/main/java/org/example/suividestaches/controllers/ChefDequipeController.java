b82bb3b0e04f9a30077a8d48e8f74fbceb806c95
ZBnada <nadazouabi5@gmail.com>
2025-05-06 14:12:14 +0200

    mise a jour de backend

--- src/main/java/org/example/suividestaches/controllers/ChefDequipeController.java
+++ src/main/java/org/example/suividestaches/controllers/ChefDequipeController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("chefDequipe")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ChefDequipeController {
     @Autowired
     private ChefDequipeService chefDequipeService;
--- src/main/java/org/example/suividestaches/repositories/ChefDequipeRepository.java
+++ src/main/java/org/example/suividestaches/repositories/ChefDequipeRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChefDequipeRepository extends JpaRepository<ChefDequipe,Long> {
+    ChefDequipe findByNom(String nom); // Recherche par nom
+
 }
